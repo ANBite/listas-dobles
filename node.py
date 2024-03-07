b88e73f6ad0777a42778205de1e44f5ed68c74a1
ANBite <124651559+ANBite@users.noreply.github.com>
2024-03-06 20:20:35 -0600

    S

---
+++ node.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import TypeVar, Generic
+T = TypeVar("T")
+
+class Node(Generic[T]):
+    def __init__(self, data: T, next_node = None):
+        self.__data = data
+        self.__next: Node | None = next_node
+
+    @property
+    def data(self):
+        return self.__data
+    @property
+    def next(self):
+        return self.__next
+
+    @next.setter
+    def next(self, new_text : Node[T]):
+        self.__next = new_text
+    def __str__(self):
+        return str(self.__data)
